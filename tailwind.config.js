628f8120211dee1f71607bf05ad8879087063b93
Sreeram A <sriramlibra0@gmail.com>
2023-03-25 08:17:23 +0530

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ['./public/index.html'],
+    theme: {
+      extend: {
+        primary : '#243c5a', 
+        secondary : {
+          100: '#243c5a',
+        }
+      },
+    },
+    plugins: [],
+  }
